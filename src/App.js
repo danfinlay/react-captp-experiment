584639f6937247dcd3ca9099c0252774b23a39fc
Dan Finlay <dan@danfinlay.com>
2021-11-11 17:30:40 -0800

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
+import makeCapTpFromStream from 'captp-stream';
 import './App.css';
+import { useState } from 'react';
+import NameComponent from './NameComponent';
+
+const makeDuplexPair = require('./duplex-socket');
+const pumpify = require('pumpify');
+console.dir(harden);
+
+// Assume two duplex streams connected to each other:
+const { clientSide, serverSide } = makeDuplexPair();
+
+// Server
+// A bootstrap should be an object with only functions on it.
+let name = 'Anon';
+const serverApi = harden({
+  getName: async () => name,
+  setName: async (newName) => { name = newName },
+});
+
+const { captpStream: serverStream }= makeCapTpFromStream('server', serverApi);
+pumpify(serverStream, serverSide, serverStream);
+
+// Client
+const { getBootstrap, E, captpStream: clientStream } = makeCapTpFromStream('client', harden({}));
+pumpify(clientStream, clientSide, clientStream);
 
 function App() {
+
+  const [bootstrap, setBootstrap] = useState(undefined);
+  const [error, setError] = useState(undefined);
+  getBootstrap()
+  .then((bootstrap) => {
+    setBootstrap(bootstrap);
+  })
+  .catch((reason) => {
+    setError(reason);
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>CapTP React Test</h1>
+      error || bootstrap ?
+      <NameComponent value={bootstrap} E={E}/> : 'Loading'
     </div>
   );
 }
